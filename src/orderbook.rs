1bfea2f53341feb8d284e93c42b8c37df01310e5
aisanaka <isanaka2@illinois.edu>
2022-04-26 06:37:36 -0500

    fixed linkledlist clone issue and updates matching

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, LinkedList};
-use crate::trade::{Trade, TradeType};
+use std::time::SystemTime;
+use crate::trade::{OrderUpdate, Trade, TradeType};
 use crate::trade::OrderType::{Limit, Market};
 use crate::trade::TradeType::{Buy, Sell};
 use rand::Rng;
@@ -10,11 +11,11 @@ pub struct OrderBook {
 
     //https://stackoverflow.com/questions/28656387/initialize-a-large-fixed-size-array-with-non-copy-types
     //index is price, LL is trades at that price
-    pub prices: [Option<LinkedList<Trade>>; 256],
+    pub prices: HashMap<u64, Option<LinkedList<Trade>>>,
     //bids- people who are buying -> should be least to greatest
-    bid_max: u8,
+    bid_max: u64,
     //asks- people who are selling -> should be greatest to least
-    ask_min: u8,
+    ask_min: u64,
 }
 
 
@@ -26,23 +27,42 @@ impl OrderBook {
         //const SIZE: usize = 18446744073709551615; 64
         //const SIZE: usize = 4294967296; 32
         //const SIZE: usize = 65536; 16
-        const SIZE: usize = 256;
-        const INIT: Option<LinkedList<Trade>> = Some(LinkedList::new());
-        let array: [Option<LinkedList<Trade>>; SIZE] = [INIT; SIZE];
+        // const SIZE: usize = 256;
+        // const INIT: Option<LinkedList<Trade>> = Some(LinkedList::new());
+        // let array: [Option<LinkedList<Trade>>; SIZE] = [INIT; SIZE];
         Self {
             book: HashMap::new(),
-            prices: array,
-            bid_max: u8::MAX,
-            ask_min: u8::MIN,
+            prices: HashMap::new(),
+            bid_max: u64::MAX,
+            ask_min: u64::MIN,
         }
     }
 
 
     pub fn remove(&mut self, order_id: u64) {
         //use order_id and get price from book
-        let price: usize = self.book[&order_id.clone()].unit_price as usize;
         //remove from price linked list
-        let mut list = self.prices[price.clone()].as_mut().unwrap();
+        //clone the list,modify the clone,  remove map entry, insert new one
+        //modify bid_max and ask_mint
+        // let mut trade = self.book[&order_id];
+        // if trade.trade_type == Buy && trade.unit_price == bid_max { //the trade being removed is the bid max
+        //     let mut list = self.prices[&self.book[&order_id].unit_price].as_ref().unwrap().clone();
+        //     let mut i = 0;
+        //     for element in list.iter_mut() {
+        //         if trade.trade_type == Buy  {
+        //             //let mut prev = list.prev();
+        //
+        //             break;
+        //         }
+        //         i += 1;
+        //     }
+        //     bid_max = trade.unit_price;
+        // } else if trade.trade_type == Sell && trade.unit_price == ask_min {
+        //     ask_min = trade.unit_price;
+        // }
+
+        //let mut list = self.prices[&self.book[&order_id].unit_price].as_ref().unwrap().clone();
+        let mut list = self.prices.get_mut(&self.book[&order_id].unit_price).unwrap().as_mut().unwrap();
         let mut i = 0;
         for element in list.iter_mut() {
             if element.order_id == order_id {
@@ -52,17 +72,31 @@ impl OrderBook {
             }
             i += 1;
         }
-        //remove from book
+        //insert new list into price levels (should override previous list) and remove from book
         self.book.remove(&order_id);
     }
 
 
-    //TODO how do i declare the helper methods properly so that they can remain private?
+
     pub fn insert(&mut self, trade: Trade) {
         //insert into hashmap and then add to the appropriate arrays linked list
         self.book.insert(trade.order_id, trade);
         //true is bid(buyers) and false is ask(seller)
-        self.prices[trade.unit_price as usize].as_mut().unwrap().push_back(trade);
+        if self.prices.contains_key(&trade.unit_price) {
+            let mut list = self.prices[&trade.unit_price].as_ref().unwrap().clone();
+            list.push_back(trade);
+            self.prices.insert(trade.unit_price.clone(), Some(list));
+        } else {
+            let mut list = LinkedList::new();
+            list.push_back(trade);
+            self.prices.insert(trade.unit_price.clone(), Some(list));
+        }
+
+        if trade.trade_type == Buy && trade.unit_price > self.bid_max{
+            self.bid_max = trade.unit_price;
+        } else if trade.trade_type == Sell && trade.unit_price < self.ask_min {
+            self.ask_min = trade.unit_price;
+        }
     }
 
 
@@ -70,52 +104,70 @@ impl OrderBook {
         if trade_input.order_id != order_id {
             return; //modify fails
         }
-        if trade_input.unit_price == 0  {
-            self.remove(order_id);
-        } else {
-            self.remove(order_id);
-            self.insert(trade_input);
-        }
+        self.remove(order_id);
+        self.insert(trade_input);
     }
 
     // //Could create and return a spread struct that contains bid and ask, not sure about the best implementation
-    pub fn top(&self) -> (u8, u8) {
+    pub fn top(&self) -> (u64, u64) {
         return (self.bid_max, self.ask_min);
     }
 
     //TODO match function -> should call add and remove appropriately
     //am currently assuming that everything is partial fill
-    // pub fn matching(&mut self, &mut incoming_trade: Trade) {
-    //     if incoming_trade.trade_type == Buy { // if its a buy order
-    //         let mut to_remove: Vec<Trade> = Vec::new();
-    //         for linked_list in self.prices[self.ask_min].as_mut().unwrap()..self.prices[self.bid_max].as_mut().unwrap() {
-    //             for current_trade in linked_list.iter_mut() {
-    //                 if current_trade.trade_type == Sell {
-    //                     if incoming_trade.qty > current_trade.qty {
-    //                         incoming_trade.qty = incoming_trade.qty - current_trade.qty;
-    //                         to_remove.push(current_trade);
-    //                     } else if current_trade.qty > incoming_trade.qty {} else {
-    //                         to_remove.push(current_trade);
-    //                         break;
-    //                     }
-    //                 }
-    //             }
-    //
-    //             if incoming_trade.qty <= 0 {
-    //                 break;
-    //             }
-    //         }
-    //         //in the case buy is greater than all of the available sells add the remaining buy to the orderbook
-    //         if incoming_trade.qty > 0 {
-    //             self.book.insert(incoming_trade);
-    //         }
-    //
-    //         //remove all of the trades as needed.
-    //         //return partially filled, filled, and status or current trade
-    //
-    //     } else { //if its a sell order
-    //     }
-    // }
+    pub fn limit_order_match(&mut self, mut incoming_trade: &mut Trade) -> Option<(Trade, Vec<Trade>)> {
+        let mut orders_filled: Vec<Trade> = Vec::new(); //remember that the last trade in this list might not be fully filled
+        if incoming_trade.trade_type == Buy { // if its a buy order
+            if incoming_trade.unit_price >= self.ask_min { //start at minimum and keep going until order is filled
+                let mut price = self.ask_min;
+                while price <= incoming_trade.unit_price {
+                    let mut linked_list = self.prices.get_mut(&price).unwrap().as_mut().unwrap();
+                    for current_trade in linked_list.iter_mut() {
+                        if current_trade.trade_type == Sell {
+                            if incoming_trade.qty > current_trade.qty {
+                                incoming_trade.qty = incoming_trade.qty - current_trade.qty;
+                                current_trade.qty = 0;
+                                orders_filled.push(current_trade.clone());
+                            } else if current_trade.qty >= incoming_trade.qty {
+                                incoming_trade.qty = 0;
+                                current_trade.qty = current_trade.qty - incoming_trade.qty;
+                                orders_filled.push(current_trade.clone());
+                                break;
+                            }
+                        }
+                    }
+                    if incoming_trade.qty == 0 {
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+        for trade in &orders_filled {
+            if trade.qty == 0 {
+                self.remove(trade.order_id);
+            }
+        }
+        if incoming_trade.qty > 0 {
+            self.insert(incoming_trade.clone());
+        } else {
+            return Some((incoming_trade.clone(), orders_filled));
+        }
+
+
+
+        return None;
+            //in the case buy is greater than all of the available sells add the remaining buy to the orderbook
+
+
+            //remove all of the trades as needed.
+            //return partially filled, filled, and status or current trade
+
+        // else { //if its a sell order
+        // }
+    }
 
     //TODO create another function that routes to add/modify/match based on order type
     //Enforce that route and maybe "fn top" are the only point of interaction w the order book
@@ -145,7 +197,7 @@ impl OrderBook {
             order_id: rng.gen::<u64>(),
             trade_type: trade_type, //buy or sell
             order_type: Market, //What type of order (market, limit, etc)
-            unit_price: rng.gen::<u8>(), //price of share in cents so we do not have to deal w super long decimals
+            unit_price: rng.gen::<u64>(), //price of share in cents so we do not have to deal w super long decimals
             qty: rng.gen::<u32>(), //number of the item they want to buy or sell
             partial_fill: true, //is partial fill of orders allowed or not
             expiration_date: rng.gen::<u32>(), //immediate fill, end_of_day, 90 day? unsure what common types there are
@@ -162,7 +214,7 @@ impl OrderBook {
             order_id: rng.gen::<u64>(),
             trade_type: input_trade_type,
             order_type: Market,
-            unit_price: rng.gen::<u8>(),
+            unit_price: rng.gen::<u64>(),
             qty: rng.gen::<u32>(),
             partial_fill: true,
             expiration_date: rng.gen::<u32>(),
@@ -179,7 +231,7 @@ impl OrderBook {
             order_id: rng.gen::<u64>(),
             trade_type: input_trade_type,
             order_type: Limit,
-            unit_price: rng.gen::<u8>(),
+            unit_price: rng.gen::<u64>(),
             qty: rng.gen::<u32>(),
             partial_fill: true,
             expiration_date: rng.gen::<u32>(),
@@ -198,7 +250,7 @@ mod tests {
         let mut rand_trade = OrderBook::generate_random_trade();
         book.insert(rand_trade);
         assert_eq!(book.book.len(), 1);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_ref().unwrap().len(), 1);
+        assert_eq!(book.prices[&rand_trade.unit_price].as_ref().unwrap().len(), 1);
     }
 
     #[test]
@@ -218,20 +270,23 @@ mod tests {
         book.insert(rand_trade);
         book.remove(rand_trade.order_id);
         assert_eq!(book.book.len(), 0);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_ref().unwrap().len(), 0);
+        assert_eq!(book.prices[&rand_trade.unit_price].as_ref().unwrap().len(), 0);
     }
 
     #[test]
-    fn single_modify_fail() {
+    fn single_modify_fail_no_list() {
         let mut book = OrderBook::new();
-        let mut rand_trade = OrderBook::generate_random_trade();
+        let mut first_trade = OrderBook::generate_random_trade();
         let mut second_trade = OrderBook::generate_random_trade();
-        book.insert(rand_trade);
-        book.modify(rand_trade.order_id, second_trade);
+        book.insert(first_trade);
+        // book.modify(rand_trade.order_id, second_trade);
+        if first_trade.order_id == second_trade.order_id {
+            second_trade.order_id += 1;
+        }
         // the original order should remain unmodified
         assert_eq!(book.book.len(), 1);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_mut().unwrap().len(), 1);
-        assert_eq!(book.prices[second_trade.unit_price as usize].as_mut().unwrap().len(), 0);
+        assert_eq!(book.prices[&first_trade.unit_price].as_ref().unwrap().len(), 1);
+        assert!(!book.prices.contains_key(&second_trade.unit_price));
     }
 
     #[test]
@@ -243,25 +298,25 @@ mod tests {
         book.insert(rand_trade);
         book.modify(rand_trade.order_id, second_trade);
         assert_eq!(book.book.len(), 1);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_mut().unwrap().len(), 0);
-        assert_eq!(book.prices[second_trade.unit_price as usize].as_mut().unwrap().len(), 1);
+        assert_eq!(book.prices[&rand_trade.unit_price].as_ref().unwrap().len(), 0);
+        assert_eq!(book.prices[&second_trade.unit_price].as_ref().unwrap().len(), 1);
     }
 
     #[test]
-    fn single_modify_zero_success() {
+    fn single_modify_price_zero_success() {
         let mut book = OrderBook::new();
         let mut rand_trade = OrderBook::generate_random_trade();
         book.insert(rand_trade);
         assert_eq!(book.book.len(), 1);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_ref().unwrap().len(), 1);
+        assert_eq!(book.prices[&rand_trade.unit_price].as_ref().unwrap().len(), 1);
 
         let mut second_trade = OrderBook::generate_random_trade();
         second_trade.order_id = rand_trade.order_id;
         second_trade.unit_price = 0;
         book.modify(rand_trade.order_id, second_trade);
-        assert_eq!(book.book.len(), 0);
-        assert_eq!(book.prices[rand_trade.unit_price as usize].as_mut().unwrap().len(), 0);
-        assert_eq!(book.prices[second_trade.unit_price as usize].as_mut().unwrap().len(), 0);
+        assert_eq!(book.book.len(), 1);
+        assert_eq!(book.prices[&rand_trade.unit_price].as_ref().unwrap().len(), 0);
+        assert_eq!(book.prices[&second_trade.unit_price].as_ref().unwrap().len(), 1);
     }
 
 
@@ -270,5 +325,9 @@ mod tests {
     //Market Sell
     //Limit Buy -> fail, success (test success w sparse and full)
     //Limit Sell -> fail, success (test success w sparse and full)
+    //everytime bid ask spread is changed send to ticket book
+    //dropcopy and gateway would get cancelled and modified trades.
+    //
+
 }
 
